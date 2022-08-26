ad4d739a1b0b9fd3ac6f85761e8ea1a29918fe3e
unknown <shenyanzheng00@gmail.com>
2022-08-26 15:33:07 -0400

    realize login and register

---
+++ calendar/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+# Create your models here.
+
+
+class Event(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.CharField(max_length=200)
+    date = models.DateField(null=True, blank=True)
+    beginning = models.IntegerField(default=0)
+    end = models.IntegerField(default=0)
