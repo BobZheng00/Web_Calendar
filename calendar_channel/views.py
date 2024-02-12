33b74e5097a40799ce2519566808322f4dfca73f
bobzheng <shenyanzheng00@gmail.com>
2024-02-11 20:34:36 -0500

    Implement event create api that responds to discord bot's request

--- calendar_channel/views.py
+++ calendar_channel/views.py
@@ -10,6 +10,7 @@ from django.forms.models import model_to_dict
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -358,8 +359,23 @@ class UserDataView(APIView):
                 'is_pined': event.is_pined,
                 'hex_color': event.hex_color
             } for event in events]
-            print(events)
             return Response(events_data, content_type='application/json')
-
+        if "create" in request.query_params:
+            input_event = InputEvents(user_id=user.user.id, event=request.query_params['event_name'],
+                                      date=datetime.datetime.strptime(request.query_params['start_date'], "%Y-%m-%d").date(),
+                                      begin_hr=request.query_params['begin_hr'],
+                                      begin_min=request.query_params['begin_min'], end_hr=request.query_params['end_hr'],
+                                      end_min=request.query_params['end_min'], description=request.query_params['description'],
+                                      is_private=request.query_params['is_private'].lower() == 'true',
+                                      is_pined=request.query_params['is_pinned'].lower() == 'true',
+                                      hex_color="#11be7c",
+                                      repeated_rule=request.query_params['repeat_pattern'],
+                                      repeated_end=datetime.datetime.strptime(request.query_params['end_date'], "%Y-%m-%d").date())
+            print(input_event)
+            valid_request, error_message = input_event.create_event()
+            if valid_request:
+                return Response({"status": "success"}, content_type='application/json', status=status.HTTP_200_OK)
+            else:
+                return Response({"status": "error", "message": error_message}, content_type='application/json', status=status.HTTP_400_BAD_REQUEST)
         data = {"username": user.extra_data}
         return Response(data)
